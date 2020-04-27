7c9044271f818ca2a5034c6d083d257ffebb9efa
liam.ward <liam.ward@audacia.co.uk>
2020-04-27 17:19:10 +0100

    Added logging client that can be used to log to the console or using an action that the user defines
    Signed-off-by: liam.ward <liam.ward@audacia.co.uk>

---
+++ Audacia.Mail.Log/LogClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Audacia.Mail.Log
+{
+    /// <summary>
+    /// Logs an email either to the console, or if configured to a sink.
+    /// </summary>
+    public class LogClient : IMailClient
+    {
+        private readonly Action<string> _loggingAction;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogClient"/> class.
+        /// </summary>
+        /// <param name="loggingAction">
+        /// Optional action that is called with the email
+        /// if you want to log it differently that via the console.
+        /// For example, you could use Serilog to log the email or you could write to a file.
+        /// </param>
+        public LogClient(Action<string> loggingAction)
+        {
+            _loggingAction = loggingAction;
+        }
+
+        /// <summary>
+        /// Log an email to the console or configured sink.
+        /// </summary>
+        /// <param name="message">The email to log.</param>
+        /// <returns>A completed Task.</returns>
+        public Task SendAsync(MailMessage message)
+        {
+            var logMessage = $"Sending email: {JsonConvert.SerializeObject(message)}";
+
+            if (_loggingAction == null)
+            {
+                Console.WriteLine(logMessage);
+            }
+            else
+            {
+                _loggingAction.Invoke(logMessage);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Dispose - this doesn't really do anything in this context, but its required by the interface.
+        /// </summary>
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- Audacia.Mail.Test.App/Program.cs
+++ Audacia.Mail.Test.App/Program.cs
@@ -1,70 +1,73 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
-using Audacia.Mail.Local;
+using Audacia.Mail.Log;
 using Audacia.Random.Extensions;
 
 namespace Audacia.Mail.Test.App
 {
-	internal static class Program
-	{
-		[SuppressMessage("ReSharper", "CA1308", Justification = "Strings are not being normalised.")]
-		private static async Task Main()
-		{
-			var localMailer = new DevMailClient(ServerType.Smtp4dev);
+    internal static class Program
+    {
+        [SuppressMessage("ReSharper", "CA1308", Justification = "Strings are not being normalised.")]
+        private static async Task Main()
+        {
+            var localMailer = new LogClient((message) =>
+            {
+                Console.WriteLine(message);
+            });
 
-			using (localMailer)
-			{
-				var random = new System.Random();
-				var iteration = 1;
+            using (localMailer)
+            {
+                var random = new System.Random();
+                var iteration = 1;
 
-				while (iteration <= 10)
-				{
-					var forenames = new[]
-					{
-						random.Boolean()
-							? random.MaleForename()
-							: random.FemaleForename(),
-						random.Boolean()
-							? random.MaleForename()
-							: random.FemaleForename()
-					};
+                while (iteration <= 10)
+                {
+                    var forenames = new[]
+                    {
+                        random.Boolean()
+                            ? random.MaleForename()
+                            : random.FemaleForename(),
+                        random.Boolean()
+                            ? random.MaleForename()
+                            : random.FemaleForename()
+                    };
 
-					var surnames = new[]
-					{
-						random.Surname(),
-						random.Surname()
-					};
+                    var surnames = new[]
+                    {
+                        random.Surname(),
+                        random.Surname()
+                    };
 
-					var message = new MailMessage
-					{
-						Format = MailFormat.Html,
-						Sender =
-						{
-							Address = $"{forenames[0]}.{surnames[0]}@example.com".ToLowerInvariant(),
-							Name = $"{forenames[0]} {surnames[0]}"
-						},
-						Recipients =
-						{
-							new MailAddress
-							{
-								Address = $"{forenames[1]}.{surnames[1]}@example.com".ToLowerInvariant(),
-								Name = $"{forenames[1]} {surnames[1]}"
-							}
-						},
-						Subject = $"Test {iteration}: {random.Word()} {random.Word()}",
-						Body = "<img src=\"https://www.audacia.co.uk/media/pkenoobu/audacia-logo-circle-blue.png\">"
-						       + $"<div><b>{random.Sentence()}</b></div>"
-					};
+                    var message = new MailMessage
+                    {
+                        Format = MailFormat.Html,
+                        Sender =
+                        {
+                            Address = $"{forenames[0]}.{surnames[0]}@example.com".ToLowerInvariant(),
+                            Name = $"{forenames[0]} {surnames[0]}"
+                        },
+                        Recipients =
+                        {
+                            new MailAddress
+                            {
+                                Address = $"{forenames[1]}.{surnames[1]}@example.com".ToLowerInvariant(),
+                                Name = $"{forenames[1]} {surnames[1]}"
+                            }
+                        },
+                        Subject = $"Test {iteration}: {random.Word()} {random.Word()}",
+                        Body = "<img src=\"https://www.audacia.co.uk/media/pkenoobu/audacia-logo-circle-blue.png\">"
+                               + $"<div><b>{random.Sentence()}</b></div>"
+                    };
 
-					await localMailer.SendAsync(message).ConfigureAwait(false);
-					Console.WriteLine($"Sent email #{iteration}");
-					await Task.Delay(1000).ConfigureAwait(false);
-					iteration++;
-				}
+                    await localMailer.SendAsync(message).ConfigureAwait(false);
+                    Console.WriteLine($"Sent email #{iteration}");
+                    await Task.Delay(1000).ConfigureAwait(false);
+                    iteration++;
+                }
 
-				await Task.Delay(int.MaxValue).ConfigureAwait(false);
-			}
-		}
-	}
+                await Task.Delay(int.MaxValue).ConfigureAwait(false);
+            }
+        }
+    }
 }
