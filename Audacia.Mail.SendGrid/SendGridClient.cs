342266f1ea668b228657763d1ef56c7e2ff3bc95
Edward Carr <edward.carr@audacia.co.uk>
2024-04-25 16:28:37 +0100

    fix: Pass correct content type e.g. text/html or text/plain and send null instead of empty array when there are no attachments.

--- Audacia.Mail.SendGrid/SendGridClient.cs
+++ Audacia.Mail.SendGrid/SendGridClient.cs
@@ -6,70 +6,77 @@ using SendGrid.Helpers.Mail;
 
 namespace Audacia.Mail.SendGrid
 {
-	/// <summary>Sends mail using the SendGrid API.</summary>
-	public class SendGridClient : IMailClient
-	{
+    /// <summary>Sends mail using the SendGrid API.</summary>
+    public class SendGridClient : IMailClient
+    {
         /// <summary>Initializes a new instance of the <see cref="SendGridClient"/> class.</summary>
         /// <param name="apiKey">The api key for making requests to SendGrid.</param>
         public SendGridClient(string apiKey)
-		{
-			_client = new global::SendGrid.SendGridClient(apiKey);
-		}
+        {
+            _client = new global::SendGrid.SendGridClient(apiKey);
+        }
 
-		/// <summary>Initializes a new instance of the <see cref="SendGridClient"/> class.</summary>
-		/// <param name="apiKey">The api key for making requests to SendGrid.</param>
-		/// <param name="defaultSender">The default sender address.</param>
+        /// <summary>Initializes a new instance of the <see cref="SendGridClient"/> class.</summary>
+        /// <param name="apiKey">The api key for making requests to SendGrid.</param>
+        /// <param name="defaultSender">The default sender address.</param>
         public SendGridClient(string apiKey, string defaultSender)
-			: this(apiKey)
-		{
-			_defaultSender = defaultSender;
-		}
+            : this(apiKey)
+        {
+            _defaultSender = defaultSender;
+        }
 
         private readonly global::SendGrid.SendGridClient _client;
         private readonly string _defaultSender = default!;
 
-		/// <inheritdoc />
+        /// <inheritdoc />
         public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-		/// <summary>Releases the unmanaged resources used by the <see cref="System.Net.Http.HttpMessageInvoker" /> and optionally disposes of the managed resources.</summary>
-		/// <param name="disposing"><see langword="true" /> to release both managed and unmanaged resources; <see langword="false" /> to releases only unmanaged resources.</param>
+        /// <summary>Releases the unmanaged resources used by the <see cref="System.Net.Http.HttpMessageInvoker" /> and optionally disposes of the managed resources.</summary>
+        /// <param name="disposing"><see langword="true" /> to release both managed and unmanaged resources; <see langword="false" /> to releases only unmanaged resources.</param>
         protected virtual void Dispose(bool disposing) { }
 
-		/// <inheritdoc />
+        /// <inheritdoc />
         public Task SendAsync(MailMessage message)
-		{
-			if (message == null) throw new ArgumentNullException(nameof(message));
+        {
+            if (message == null) throw new ArgumentNullException(nameof(message));
 
-			if (string.IsNullOrWhiteSpace(message.Sender?.Address) &&
+            if (string.IsNullOrWhiteSpace(message.Sender?.Address) &&
                !string.IsNullOrWhiteSpace(_defaultSender))
             {
                 message.Sender = new MailAddress(_defaultSender);
             }
 
-			var sendGridMessage = new SendGridMessage
-			{
-                Contents = new List<Content> { new Content { Type = message.Format.ToString(), Value = message.Body } },
+            var sendGridMessage = new SendGridMessage
+            {
+                Contents = new List<Content>
+                {
+                    new Content { Type = $"text/{message.Format}", Value = message.Body }
+                },
                 From = new EmailAddress(message.Sender?.Address, message.Sender?.Name),
-				Subject = message.Subject,
-				Attachments = message.Attachments.Select(a => new Attachment
-				{
-					Content = Convert.ToBase64String(a.Bytes.ToArray())
-				}).ToList()
-			};
+                Subject = message.Subject
+            };
 
-			var recipients = message.Recipients.Select(r => new EmailAddress
-			{
-				Name = r.Name,
-				Email = r.Address
-			}).ToList();
+            if (message.Attachments.Any())
+            {
+                sendGridMessage.Attachments = message.Attachments.Select(a => new Attachment
+                {
+                    Content = Convert.ToBase64String(a.Bytes.ToArray())
+                }).ToList();
+            }
 
-			sendGridMessage.AddTos(recipients);
+            var recipients = message.Recipients.Select(r => new EmailAddress
+            {
+                Name = r.Name,
+                Email = r.Address
+            }).ToList();
 
-			return _client.SendEmailAsync(sendGridMessage);
-		}
-	}
+            sendGridMessage.AddTos(recipients);
+
+            return _client.SendEmailAsync(sendGridMessage);
+        }
+    }
 }
